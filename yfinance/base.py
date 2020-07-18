457c54a7bec462904cf8e2a344b0dd802a87d7f6
Riccardo Vincenzo Vincelli <reimone@gmail.com>
2020-07-18 14:31:13 +0200

    (corrects for list)

--- yfinance/base.py
+++ yfinance/base.py
@@ -282,8 +282,8 @@ class TickerBase():
         # holders
         url = "{}/{}/holders".format(self._scrape_url, self.ticker)
         holders = _pd.read_html(url)
-        self._major_holders = holders.get(0)
-        self._institutional_holders = holders.get(1)
+        self._major_holders = holders[0] if len(holders) >= 1 else None
+        self._institutional_holders = holders[1] if len(holders) >= 2 else None
         if 'Date Reported' in self._institutional_holders:
             self._institutional_holders['Date Reported'] = _pd.to_datetime(
                 self._institutional_holders['Date Reported'])
